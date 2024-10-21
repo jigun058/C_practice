240c890a9e6c45bc22907c6fb51a47763f7a1c3c
taegwonbrew <jigun058@gmail.com>
2024-10-21 11:53:50 +0900

    sdfjkljasd

--- myPractice/tempCodeRunnerFile.c
+++ myPractice/tempCodeRunnerFile.c
@@ -1,22 +1,2 @@
-List Sorted;
-    // init(&Sorted);
 
-    // insert(&Sorted, 0, delete(&L, 0));
-
-    // count = 0;
-    // while(count < 10){
-    //     int temp = delete(&L, 0);
-    //     for(int i=0; i<10; i++){
-    //         if(temp < getEntry(&Sorted, i)){
-    //             insert(&Sorted, i, temp);
-    //             count += 1;
-    //             break;
-    //         }
-    //         if(i==count){
-    //             insert(&Sorted, i, count);
-    //         }
-    //     }
-    // }
-
-    // for(int i=0; i<10; i++)
-    //     printf("%d\n", delete(&Sorted, 0));
+    printf("%c\n", pop(&S));
