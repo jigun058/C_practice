daa133e2958d6ec0ce7a1b0835f543078f1fbb73
jigun058 <jigun058@gmail.com>
2024-10-29 00:39:01 +0900

    1411515

--- test.c
+++ test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(){
-    printf("Hello\n");
-    return 0;
+typedef struct _TEST_T {
+ int i;
+ short s;
+ int j;
+}TEST_T;
+int main(void) {
+ printf("size of TEST_T: %d\n", sizeof(TEST_T));
+ return 0;
 }
